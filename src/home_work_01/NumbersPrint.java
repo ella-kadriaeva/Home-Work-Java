f47e2236be7e811fc39016f9f3cf5e9ae8d2ffe1
Ella-Kadriaieva <ella.kadriaieva@gmail.com>
2024-09-28 23:27:52 +0300

    fix hw-1

--- src/home_work_01/NumbersPrint.java
+++ src/home_work_01/NumbersPrint.java
@@ -2,9 +2,9 @@ package home_work_01;
 
 public class NumbersPrint {
     public static void main(String[] args) {
-        int number = 777;
+        int number = 526;
         int firstNumber= number / 100;
-        int secondNumber =(number - firstNumber *100) /10;
+        int secondNumber =number % 100 /10;
         int thirdNumber = number - firstNumber *100 - secondNumber *10;
         System.out.printf("%d, %d, %d", firstNumber, secondNumber, thirdNumber);
     }
